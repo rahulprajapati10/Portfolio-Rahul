f3e174f866c6abab11d669eb0a586a0047077439
rahul prajapati <rahulprajapati2930@gmail.com>
2025-02-13 00:08:50 +0530

    added one project

--- src/Components/WorkCardData.jsx
+++ src/Components/WorkCardData.jsx
@@ -2,15 +2,16 @@ import pro1 from '../assets/img1.avif'
 import pro2 from '../assets/img2.jpg'
 import pro3 from '../assets/img5.avif'
 import project from '../assets/Screenshot.png'
+import project2 from '../assets/scr.png'
 
 
 const WorkCardData = [
     {
-        imgsrc: project,
+        imgsrc: project2,
         title: "Login Form Desing",
-        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow ! I really can do this foe over 100,000 online entrepreneurs . Best -selling marketing courses ",
-        view: "https://rahulprajapati10.github.io/project_live_demo/",
-        Source: "https://github.com/rahulprajapati10/project_live_demo.git"
+        text: "This project is a frontend-only login and sign-up page designed with a modern UI and smooth user experience. It features a clean and responsive layout, making it easy to integrate into any web application. ",
+        view: "https://rahulprajapati10.github.io/Authentication_web_page/",
+        Source: ""
 
     },
 
@@ -26,16 +27,16 @@ const WorkCardData = [
         imgsrc: project,
         title: "Login Form Desing",
         text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow ! I really can do this foe over 100,000 online entrepreneurs . Best -selling marketing courses ",
-        view: "https://rahulprajapati10.github.io/project_live_demo/",
-        Source: "https://github.com/rahulprajapati10/project_live_demo.git"
+        view: "",
+        Source: ""
     },
 
     {
         imgsrc: pro3,
         title: "Ecommerce Website design",
         text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow ! I really can do this foe over 100,000 online entrepreneurs . Best -selling marketing courses ",
-        view: "https://www.youtube.com/watch?v=hgjQTEL938w",
-        Source: "https://github.com/rahulprajapati10/project_live_demo.git"
+        view: "",
+        Source: ""
 
     },
 
@@ -43,8 +44,8 @@ const WorkCardData = [
         imgsrc: pro3,
         title: "Ecommerce Website design",
         text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow ! I really can do this foe over 100,000 online entrepreneurs . Best -selling marketing courses ",
-        view: "https://www.youtube.com/watch?v=hgjQTEL938w",
-        Source: "https://github.com/rahulprajapati10/project_live_demo.git"
+        view: "",
+        Source: ""
        
     },
 
@@ -52,8 +53,8 @@ const WorkCardData = [
         imgsrc: pro3,
         title: "Ecommerce Website design",
         text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow ! I really can do this foe over 100,000 online entrepreneurs . Best -selling marketing courses ",
-        view: "https://www.youtube.com/watch?v=hgjQTEL938w",
-        Source: "https://github.com/rahulprajapati10/project_live_demo.git"
+        view: "",
+        Source: ""
     },
 ]
 
